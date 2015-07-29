ed9b60fe63ce74b0659f1e41e4471937d238ebc5
OstlerDev <ostlerdev@me.com>
2015-07-29 17:54:15 -0600

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Howler
+
+A simple plugin to add howling to servers. Just type ``/howl`` and you are off!
+
+## Commands
+
+``/howl`` Howl at your current location up to the sky.
---
+++ src/com/skylarostler/howler/Howler.java
@@ -0,0 +1,34 @@
+package com.skylarostler.howler;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Howler extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("howl")) { // If the player typed /howl then do the following...
+    		if (!(sender instanceof Player)) {
+    			sender.sendMessage("This command can only be run by a player.");
+    		} else {
+    			Player player = (Player) sender;
+    			// Howl
+    			player.getWorld().playSound(player.getLocation(), Sound.WOLF_HOWL, 10.0F, 1.0F);
+    			this.getServer().broadcastMessage(ChatColor.GOLD + player.getDisplayName() + " howls up to the sky");
+    		}
+    		return true;
+    	}
+    	return false; 
+    }
+}
